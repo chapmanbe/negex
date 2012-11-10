eded30d740a46160ba7a61bf379d71865dc7ca17
chapmanbe <chapmanbe@840a737e-d840-11dd-93da-dbb4cac96d7c>
2012-11-10 20:19:24 +0000

    modifying scripts for both posix and windows installations

---
+++ trunk/CherryConText/trunk/createCherryConTextVE-win.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import virtualenv, textwrap
+
+output = virtualenv.create_bootstrap_script(textwrap.dedent("""
+import os
+import sys
+
+import subprocess
+import urllib2
+from zipfile import ZipFile
+import StringIO
+
+user_home = os.path.expanduser("~")
+cfiles = os.path.join(user_home,"CherryConText.zip")
+
+def after_install(options, home_dir):
+    if( os.name == 'posix' ):
+        print "Installing on a posix system"
+        bloc = 'bin'
+    else:
+        print "Assuming installation on a Windows system"
+        bloc = 'Scripts'
+    print "Installing virtualenv"
+    subprocess.call([join(home_dir,bloc,'pip'), 'install', 'cherrypy'])
+    subprocess.call([join(home_dir,bloc,'pip'), 'install', 'django'])
+    subprocess.call([join(home_dir,bloc,'pip'), 'install', 'pyConTextNLP'])
+    url = urllib2.urlopen("http://negex.googlecode.com/files/CherryConText.zip")
+    zipfile = ZipFile(StringIO.StringIO(url.read()))
+    zipfile.extractall(user_home)
+"""))
+f = open('cherryConText-bootstrap2-win.py','w').write(output)
+
+
+
+
+
--- trunk/CherryConText/trunk/runme.py
+++ trunk/CherryConText/trunk/runme.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import subprocess
 import os
+
+user_home = os.path.expanduser("~")
 if( os.name == 'posix' ):
     bloc = 'bin'
 else:
     bloc = 'Scripts'
-
+os.chdir(os.path.join(user_home,"CherryConText"))
 # I need to change this script to invoke the virtualenv first
-subprocess.call("""%s __init__.py"""%os.path.join("CCTVE",bloc,"python"),shell=True)
+subprocess.call("""%s %s"""%(os.path.join("CCTVE",bloc,"python"),
+                             "__init__.py"),shell=True)
